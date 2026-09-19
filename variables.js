7e4ec522c063c5f29e0ff65b301deb8bc763d49d
Vaibhav-Rohokale <vaibhavrohokale03@gmail.com>
2026-09-19 22:41:15 +0530

    Add variable examples

---
+++ variables.js
@@ -0,0 +1,16 @@
+const accountNo = 65425
+let accountHolder = "Donald Trump"
+var accountEmail = "donald@usa.com"
+
+accountCountry = "USA"
+
+// accountNo = 14552  const cannot be changed
+
+accountHolder = "putin";
+accountEmail = "putin@russia.com"
+accountCountry = "Russia"
+
+let accountState;
+
+
+console.table([accountNo, accountHolder, accountEmail, accountCountry, accountState])
